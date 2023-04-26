391e127fe4d4dbd712a1e6b62ce75ba8095a7a40
Jagdish-Salunkhe <117559650+Jagdish-Salunkhe@users.noreply.github.com>
2023-04-27 00:59:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // your code here
+<script>
+		const nameField = document.getElementById("name");
+		const yearField = document.getElementById("year");
+		const button = document.getElementById("button");
+		const urlField = document.getElementById("url");
+		
+		button.addEventListener("click", function() {
+			let nameValue = encodeURIComponent(nameField.value);
+			let yearValue = encodeURIComponent(yearField.value);
+			let queryString = `?name=${nameValue}&year=${yearValue}`;
+			urlField.textContent = `https://localhost:8080/${queryString}`;
+		});
+	</script>
